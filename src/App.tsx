fe771667a96296d19eae48bfecb85c8293243330
Sushank Ghimire <mcoder006@gmail.com>
2024-04-06 13:33:09 +0545

    Added the system keyboard click event Listener

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import HangmanBody from "./components/HangmanBody";
 import HangmanWords from "./components/HangmanWords";
 import Keyboard from "./components/Keyboard";
 import ThemeSwitcher from "./components/ThemeSwitcher";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import words from "./wordList.json";
 
 const App = () => {
    const [wordGuess, setWordGuess] = useState(() => {
-    return 'hello'
-    //  return words[Math.floor(Math.random() * words.length)];
+     return words[Math.floor(Math.random() * words.length)];
    });
 
    const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
@@ -17,7 +16,36 @@ const App = () => {
      (letter) => !wordGuess.includes(letter)
    );
 
-  //  console.log(incorrectLetters);
+   const addGuessedLetter = ( keyletter: string ) => {
+    if(guessedLetters.includes(keyletter)) {
+      return 
+    }
+    
+    setGuessedLetters(currentLetters => [...currentLetters, keyletter])
+   }
+
+   useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+
+      if(!key.match(/^[a-z]$/)) {
+        return
+      }
+      else {
+        e.preventDefault();
+        addGuessedLetter(key);
+      }
+    }
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    }
+    
+   }, [])
+
+   
    
   return (
     <div className="flex flex-col items-center justify-center w-screen min-h-screen space-y-4 text-black bg-gradient-to-br dark:from-slate-700 dark:to-slate-600 from-slate-500 to-slate-100 dark:text-white md:text-2xl">
